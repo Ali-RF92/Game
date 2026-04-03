e632379f46700c7a01eb74a5df55bf99919587d6
Ali Rasouli Fard <ali.rasoulifard.edu@gmail.com>
2026-04-03 18:17:56 +0200

    show display - close display

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
 import pygame
+import sys
+
+pygame.init()
+game_display = pygame.display.set_mode((600,400))
+pygame.display.set_caption("My First Game")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()       
+
