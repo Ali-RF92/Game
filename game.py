4d178f42f5dea6fd698425d80223cf364b5a585a
Ali Rasouli Fard <ali.rasoulifard.edu@gmail.com>
2026-04-03 18:33:36 +0200

    create new file

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 import pygame
 import sys
 from init import *
+from constants import *
+
 
 
 while True:
+    game_display.fill(white)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
